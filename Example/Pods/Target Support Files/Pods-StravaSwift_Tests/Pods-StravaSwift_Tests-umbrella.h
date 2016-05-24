dddc0d5c454e15756b1fb92e3b4e8d9852de1c8d
Matthew Clarkson <matt@hilenium.com>
2016-05-24 15:00:10 +1000

    added file

---
+++ Example/Pods/Target Support Files/Pods-StravaSwift_Tests/Pods-StravaSwift_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_StravaSwift_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StravaSwift_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/StravaSwift/StravaSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StravaSwift : NSObject
+@end
+@implementation PodsDummy_StravaSwift
+@end
