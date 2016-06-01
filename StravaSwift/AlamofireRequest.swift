1b4269c09491419c6ae80e01dcb8cffbf9cc7945
Matthew Clarkson <matt@hilenium.com>
2016-06-01 13:51:05 +1000

    removed duplicate code

--- StravaSwift/AlamofireRequest.swift
+++ StravaSwift/AlamofireRequest.swift
@@ -49,45 +49,53 @@ extension Request {
 
 extension Request {
     
+    typealias SerializeResponse = (NSURLRequest?, NSHTTPURLResponse?, NSData?, NSError?)
+    
+    private static func parseResponse(info: SerializeResponse) -> (Result<AnyObject,NSError>?, NSError?) {
+        let (request, response, data, error) = info
+        
+        guard let _ = data else {
+            let error = generateError("Data could not be serialized. Input data was nil.")
+            return (nil, error)
+        }
+        
+        let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+        let result = JSONResponseSerializer.serializeResponse(request, response, data, error)
+        
+        return (result, nil)
+    }
+    
+    private static func generateError(string: String) -> NSError {
+        return Error.errorWithCode(.DataSerializationFailed, failureReason: string)
+    }
+    
     static func StravaSerializer<T: Strava>(keyPath: String?) -> ResponseSerializer<T, NSError> {
         return ResponseSerializer { request, response, data, error in
+            let (result, e) = parseResponse(request, response, data, error)
             
-            guard let _ = data else {
-                let failureReason = "Data could not be serialized. Input data was nil."
-                let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
-                return .Failure(error)
+            if let e = e {
+                return .Failure(e)
             }
             
-            let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
-            let result = JSONResponseSerializer.serializeResponse(request, response, data, error)
-            
-            if let json = result.value {
+            if let json = result?.value {
                 let object = T.init(JSON(json))
                 return .Success(object)
             }
-            
-            let failureReason = "StravaSerializer failed to serialize response."
-            let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
-            return .Failure(error)
+
+            return .Failure(generateError("StravaSerializer failed to serialize response."))
         }
     }
     
     static func StravaArraySerializer<T: Strava>(keyPath: String?) -> ResponseSerializer<[T], NSError> {
         return ResponseSerializer { request, response, data, error in
-            guard error == nil else {
-                return .Failure(error!)
+            
+            let (result, e) = parseResponse(request, response, data, error)
+            
+            if let e = e {
+                return .Failure(e)
             }
             
-            guard let _ = data else {
-                let failureReason = "Data could not be serialized. Input data was nil."
-                let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
-                return .Failure(error)
-            }
-            
-            let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
-            let result = JSONResponseSerializer.serializeResponse(request, response, data, error)
-            
-            if let json = result.value {
+            if let json = result?.value {
                 var results:[T] = []
                 JSON(json).array?.forEach {
                     results.append(T.init($0))
@@ -95,10 +103,8 @@ extension Request {
                 
                 return .Success(results)
             }
-            
-            let failureReason = "StravaSerializer failed to serialize response."
-            let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
-            return .Failure(error)
+
+            return .Failure(generateError("StravaSerializer failed to serialize response."))
         }
     }
 }
