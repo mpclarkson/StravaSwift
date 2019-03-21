12d88e08260b4e4f01308b1e2678a665df689f95
FleetPhil <15308797+FleetPhil@users.noreply.github.com>
2019-03-21 16:19:50 +0000

    Updated ActivityType to Strava values
    All now initial capital letters on names e.g. 'ride' now 'Ride'

--- StravaSwift/ModelEnums.swift
+++ StravaSwift/ModelEnums.swift
@@ -111,9 +111,10 @@ public enum WorkoutType: Int {
  **/
 public enum ActivityType: String {
     case
-    ride, run, swim, hike, walk, alpineSki, backcountrySki, canoeing, crossfit, eBikeRide, elliptical,
-    iceSkate, inlineSkate, kayaking, yoga, kitesurf, nordicSki, rockClimbing, rollerSki, rowing,
-    snowboard, snowshoe, stairStepper, standUpPaddling, surfing, virtualRide, weightTraining, windsurf, workout
+    AlpineSki, BackcountrySki, Canoeing, Crossfit, EBikeRide, Elliptical, Golf, Handcycle, Hike, IceSkate, 
+ InlineSkate, Kayaking, Kitesurf, NordicSki, Ride, RockClimbing, RollerSki, Rowing, Run, Sail, Skateboard, 
+ Snowboard, Snowshoe, Soccer, StairStepper, StandUpPaddling, Surfing, Swim, Velomobile, VirtualRide, VirtualRun, 
+ Walk, WeightTraining, Wheelchair, Windsurf, Workout, Yoga
 }
 
 /**
