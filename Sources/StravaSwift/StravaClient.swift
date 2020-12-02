8ae7e6e06fa9c572bec457c8312c5d9aa31ea3e2
Stan Ostrovskiy <stanislav.ostrovskiy@instacart.com>
2020-12-02 10:38:11 -0500

    update client fallback url

--- Sources/StravaSwift/StravaClient.swift
+++ Sources/StravaSwift/StravaClient.swift
@@ -41,8 +41,7 @@ open class StravaClient: NSObject {
             "scope" : (config?.scopes ?? []).map { $0.rawValue }.joined(separator: ","),
             "state" : "ios" as AnyObject,
             "approval_prompt" : config?.forcePrompt ?? true ? "force" : "auto",
-            "response_type" : "code",
-            "fallbackUrl": config?.fallbackUrl ?? ""
+            "response_type" : "code"
         ]
     }
 
