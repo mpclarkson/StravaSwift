025717dc3144744a1ea016e3bf1eee09f4c5236d
Mike Chartier <ehmjaysee@icloud.com>
2021-12-06 13:43:49 -0600

    fix travis

---
+++ Example/StravaSwift/ActivityDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityDetailVC.swift
+//  StravaSwift_Example
+//
+//  Created by Michael Chartier on 12/6/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ActivityDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
